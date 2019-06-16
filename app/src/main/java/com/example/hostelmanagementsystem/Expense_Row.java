4743c62294b52842125a8792d5c68a8f5d6cc6ae
Thamina33 <37588746+Thamina33@users.noreply.github.com>
2019-06-16 14:37:21 -0700

    V1.0.0

---
+++ app/src/main/java/com/example/hostelmanagementsystem/Expense_Row.java
@@ -0,0 +1,14 @@
+package com.example.hostelmanagementsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Expense_Row extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expense__row);
+    }
+}
--- app/src/main/java/com/example/hostelmanagementsystem/HomePage.java
+++ app/src/main/java/com/example/hostelmanagementsystem/HomePage.java
@@ -1,14 +1,28 @@
 package com.example.hostelmanagementsystem;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePage extends AppCompatActivity {
 
+
+    TextView add_stdn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        add_stdn = (TextView)findViewById(R.id.add_stdn);
+        add_stdn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent intent = new Intent (HomePage.this, StudentInformation.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
